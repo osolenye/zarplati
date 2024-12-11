cbafb4256947edeac8a6429749f36023e87c3a23
osolenye <116378424+osolenye@users.noreply.github.com>
2024-12-11 21:57:41 +0600

    postgre

--- zarplati/settings.py
+++ zarplati/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'zarplati.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'zarplati',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Или IP-адрес вашего сервера
+        'PORT': '5432',       # Стандартный порт PostgreSQL
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
