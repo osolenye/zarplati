6f047493d45fbd64f0f0e43872634c98f1ab7ded
osolenye <116378424+osolenye@users.noreply.github.com>
2024-12-12 01:11:28 +0600

    swagger added

--- zarplati/urls.py
+++ zarplati/urls.py
@@ -17,6 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Настройка для Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+      description="API Documentation",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourdomain.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
 ]
