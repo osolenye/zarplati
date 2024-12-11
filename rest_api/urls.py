e021cf5e0bb7798cfada613d438ab89f479172e1
osolenye <116378424+osolenye@users.noreply.github.com>
2024-12-12 01:55:57 +0600

    addmin registration

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CompanyViewSet, AdminViewSet, WorkerViewSet
+from .views import CompanyViewSet, AdminViewSet, WorkerViewSet, AdminRegistrationViewSet, ObtainTokenPairView
 
 router = routers.DefaultRouter()
 router.register(r'companies', CompanyViewSet)
@@ -9,4 +9,6 @@ router.register(r'workers', WorkerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', AdminRegistrationViewSet.as_view({'post': 'create'})),
+    path('login/', ObtainTokenPairView.as_view(), name='token_obtain_pair'),
 ]
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,6 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from .models import Company, Admin, Worker
 from .serializers import CompanySerializer, AdminSerializer, WorkerSerializer
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
@@ -16,3 +22,20 @@ class WorkerViewSet(viewsets.ModelViewSet):
     queryset = Worker.objects.all()
     serializer_class = WorkerSerializer
     permission_classes = [permissions.AllowAny]
+
+
+
+class AdminRegistrationViewSet(viewsets.ViewSet):
+    permission_classes = (permissions.AllowAny,)
+
+    def create(self, request):
+        serializer = AdminSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            return Response({'token': access_token}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ObtainTokenPairView(TokenObtainPairView):
+    pass
