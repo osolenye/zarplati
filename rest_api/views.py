b34aecd1aaf7ee7e48e49e360dd0312c06263a38
osolenye <116378424+osolenye@users.noreply.github.com>
2024-12-12 01:41:35 +0600

    company admin worker crud

--- rest_api/views.py
+++ rest_api/views.py
@@ -10,9 +10,9 @@ class CompanyViewSet(viewsets.ModelViewSet):
 class AdminViewSet(viewsets.ModelViewSet):
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
 class WorkerViewSet(viewsets.ModelViewSet):
     queryset = Worker.objects.all()
     serializer_class = WorkerSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
